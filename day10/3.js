1e0e80ea98ee6c79bb34551bb91bd14f844148c9
sumedhtalekar29 <65176658+sumedhtalekar29@users.noreply.github.com>
2020-08-17 19:59:42 +0530

    day 10 Assignments

---
+++ day10/3.js
@@ -0,0 +1,11 @@
+let likeMe = function (btnElement) {
+    
+    let likeCountString = btnElement.children[0].innerHTML;
+  
+    
+    let likeCount = parseInt(likeCountString);
+  
+    likeCount++;
+  
+    btnElement.children[0].innerHTML = likeCount;
+  };
